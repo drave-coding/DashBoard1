8c274953d0d55399b9b7d3016015fd95a2b71456
Abhishek Das <abhishek2022.work@gmail.com>
2023-07-14 19:29:29 +0530

    app

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        pieceName: 'Shading Works',
+        pieceNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        pieceName: 'Father Bason',
+        pieceNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        pieceName: 'Monalisa Doted',
+        pieceNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
